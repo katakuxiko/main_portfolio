12dfd77d19c1290c6124c2f3b1fc9ea6eda4b98a
Katakuxiko <katakuxiko@gmail.com>
2023-04-02 13:21:37 +0600

    view on mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -19,8 +19,8 @@ const Computers = ({ isMobile }) => {
 			/>
 			<primitive
 				object={computer.scene}
-				scale={0.75}
-				position={[0, -3.25, -1.5]}
+				scale={isMobile ? 0.7 : 0.75}
+				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
 				rotation={[-0.01, -0.2, -0.1]}
 			/>
 		</mesh>
